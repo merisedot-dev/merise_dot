b088676493092dca7df5ddcfd9066b0d73da7a47
kheldae <taho.gougeon@gmail.com>
2025-09-29 10:47:34 +0200

    finished edition operation on field

--- merise_dot/commands/mcd/ops/fields/fd_edt.py
+++ merise_dot/commands/mcd/ops/fields/fd_edt.py
@@ -5,6 +5,11 @@ from merise_dot.dot import mkrt
 
 # modifcation names
 _MODS: list[str] = ["change name", "change type", "make primary"]
+_PRIMARY_RULES: dict[str | int] = {
+    "make primary": 1,
+    "make not primary": -1,
+    "abort change": 0
+}
 
 
 def edit_field_op(entity: Entity) -> None:
@@ -20,6 +25,18 @@ def edit_field_op(entity: Entity) -> None:
         if n_name in entity._fields.keys():
             rprint(f"Another field is already named {n_name}")
             return
+        f_t, f_p = entity.get_field(f_name)
+        # do the swap
+        entity.add_field(n_name, f_t, f_p)
+        entity.delete_field(f_name)
     elif mod == _MODS[1]:
         n_type: str = questionary.question("Field's new type :").ask()
-        # TODO sanitize
+        # just raw input for now
+        entity.edit_field(f_name, n_type, 0)
+    else: # _MODS[2]
+        n_pr: int = _PRIMARY_RULES[questionary.select(
+            "What to make the field into ?",
+            choices=_PRIMARY_RULES.keys()).ask()]
+        # do the change
+        f_t, _ = entity.get_field(f_name)
+        entity.edit_field(f_name, f_t, n_pr)
