c673b8c84d65e83bd830a1e44c531f1324ffdd8a
kheldae <taho.gougeon@gmail.com>
2025-10-10 16:49:51 +0200

    mld test book

---
+++ merise_dot/model/mld/entity.py
@@ -0,0 +1,26 @@
+# Extra constants, please do not touch
+_REGULAR_CODE: int = 0
+_PK_CODE: int = 1
+_FK_CODE: int = 2
+
+
+class MLDEntity:
+    """MLD_specific overlay on the old MCD Entity.
+    This is meant to provide a unified interface for the old types, now that the
+    user wants MLD conversion.
+
+    MCD should remain the main way for a user to interact with a database graph.
+    """
+
+    def __init__(self, name: str) -> None:
+        self._name: str = name
+        self._fields: dict[str | (str, int)] = {}
+
+    def add_field(
+            self, name: str, f_type: str, status: int = _REGULAR_CODE) -> None:
+        """Adding a new field to the entity.
+
+        :param name:
+        :param f_type:
+        :param status:
+        """
